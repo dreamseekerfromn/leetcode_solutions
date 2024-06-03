a306590734a69eb6565de92344306a3403001460
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-06-03 19:29:21 -0400

    Time: 54 ms (69.41%) | Memory: 51.8 MB (67.44%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,26 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    const mid = Math.floor(nums.length / 2);
-
-    if(nums[mid] == target){
-        return mid;
-    }
-
-    if(nums[mid] < target){
-        
-        for(let i = mid; i < nums.length; i++){
-            if(nums[i] == target){
-                return i;
-            };
+    let mid = Math.floor(nums.length / 2);    
+    let left = 0;
+    let right = nums.length;
+    do{
+        if(nums[mid] == target){
+            return mid;
         }
-    } else {
-        for(let i = 0; i < mid; i++){
-            if(nums[i] == target){
-                return i;
-            };
+        if(nums[mid] < target){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
-    }  
-
+        mid = Math.floor((right + left)/2);
+    } while(left <= right)
+    
     return -1;  
 };
