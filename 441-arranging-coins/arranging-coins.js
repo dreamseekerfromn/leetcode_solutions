4d420ade0bcd980715e57caef65ddcc3cad53c2f
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-06-04 19:14:36 -0400

    Time: 89 ms (54.51%) | Memory: 53 MB (77.19%) - LeetSync

--- 441-arranging-coins/arranging-coins.js
+++ 441-arranging-coins/arranging-coins.js
@@ -4,13 +4,15 @@
  */
 var arrangeCoins = function(n) {
     let row = 0;
-    for(let i = 1; n >= 1; i++){
+
+    for(let i = 1; n >= i; i++){
         n -= i;
-        
+
+        /*
         if(n < 0){
             return row;
         }
-
+        */
         row++;
     }
 
