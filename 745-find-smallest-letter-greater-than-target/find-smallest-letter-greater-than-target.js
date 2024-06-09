75ef1e82d50e8564db56a272e10124100b3105ae
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-06-09 16:31:09 -0400

    Time: 58 ms (45.41%) | Memory: 50.7 MB (80.72%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
@@ -4,26 +4,31 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    let index1 = Math.floor(letters.length / 2);
-    let compare = letters[index1].charCodeAt(0) - target.charCodeAt(0)
-    if(compare == 1){
-        
-        return letters[index1];
+    if(letters[0] > target){
+        return letters[0];
     }
 
-    if(compare <= 0){
-        for(let i = index1 + 1; i < letters.length; i++){
-            if(letters[i] > target){
-                return letters[i];
-            }
+    let mid = Math.floor(letters.length / 2);
+    let start = 1;
+    let end = letters.length - 1;
+    do{
+        if(letters[mid].charCodeAt(0) - target.charCodeAt(0) == 1){
+            return letters[mid];
         }
-    } else {
-        for(let i = 0; i <= index1; i++){
-            if(letters[i] > target){
-                return letters[i];
-            }
-        }
-    }
 
+        if(letters[mid] <= target){
+            start = mid + 1;
+            mid = Math.floor((start + end) / 2);
+        } else {
+            end = mid;
+            for(let i = start; i <= end; i++){
+                if(letters[i] > target){
+                    return letters[i];
+                }
+            }
+            return letters[0];
+        }
+    } while(start <= end);
     return letters[0];
+
 };
