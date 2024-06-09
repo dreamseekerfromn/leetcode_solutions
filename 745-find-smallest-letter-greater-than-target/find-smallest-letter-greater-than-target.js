cb13b0d631270edf2d9459a10afb6df2162ba4e7
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-06-09 15:49:40 -0400

    Time: 50 ms (87.56%) | Memory: 50.7 MB (72.01%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
@@ -4,9 +4,24 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    for(let i = 0; i < letters.length; i++){
-        if(target < letters[i]){
-            return letters[i];
+    let index1 = Math.floor(letters.length / 2);
+    let compare = letters[index1].charCodeAt(0) - target.charCodeAt(0)
+    if(compare == 1){
+        
+        return letters[index1];
+    }
+
+    if(compare <= 0){
+        for(let i = index1 + 1; i < letters.length; i++){
+            if(letters[i] > target){
+                return letters[i];
+            }
+        }
+    } else {
+        for(let i = 0; i <= index1; i++){
+            if(letters[i] > target){
+                return letters[i];
+            }
         }
     }
 
