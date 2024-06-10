6967edb112db7e3f89e421cd258f6b32fd50032e
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-06-10 17:14:25 -0400

    Time: 79 ms (38.97%) | Memory: 52.3 MB (94.09%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -5,21 +5,15 @@
  */
 var findWordsContaining = function(words, x) {
     let result = [];
-    let set = [];
-    for(let i = 0; i < words.length; i++){
-        let tempSet = new Set();
-        for(let j = 0; j < words[i].length; j++){
-            tempSet.add(words[i][j]);
-        }
-        set.push(tempSet);
-    }
 
-    for(let k = 0; k < set.length; k++){
-        if(set[k].has(x)){
-            result.push(k);
+    for(let i = 0; i < words.length; i++){
+        for(let j = 0; j < words[i].length; j++){
+            if(words[i][j] == x){
+                result.push(i);
+                break;
+            }
         }
     }
 
     return result;
-
 };
