a7a96ac6e76b72749956fc5dfd550176513b7f2e
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-06-17 08:18:24 -0400

    Time: 59 ms (60.27%) | Memory: 49.7 MB (57.53%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.js
+++ 633-sum-of-square-numbers/sum-of-square-numbers.js
@@ -7,9 +7,9 @@ var judgeSquareSum = function(c) {
     let index2 = Math.floor(Math.sqrt(c));
     
     while(index1 <= index2){
-        if(Math.pow(index1, 2) + Math.pow(index2, 2) == c){
+        if(index1 * index1 + index2 * index2 == c){
             return true;
-        } else if(Math.pow(index1, 2) + Math.pow(index2, 2) < c){
+        } else if(index1 * index1 + index2 * index2 < c){
             index1++;
         } else {
             index2--;
