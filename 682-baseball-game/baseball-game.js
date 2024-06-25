edd7331c002c2f2e24f0ffed1fd4fc3d2109a74f
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-06-25 19:25:06 -0400

    Time: 259 ms (5.82%) | Memory: 59.2 MB (5.56%) - LeetSync

--- 682-baseball-game/baseball-game.js
+++ 682-baseball-game/baseball-game.js
@@ -7,7 +7,7 @@ var calPoints = function(operations) {
     for(let i = 0; i < operations.length; i++){
         console.log(record)
         if(operations[i] == 'D'){
-            record.push(record.at(-1) * 2);
+            record.push(record[record.length - 1] * 2);
         } else if(operations[i] == 'C'){
             record.pop();
         } else if(operations[i] == '+'){
