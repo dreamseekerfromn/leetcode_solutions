380a5e54d9e5b60b81831bbd1ad410660dffc5f5
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-06-29 15:45:03 -0400

    Time: 0 ms (100.00%) | Memory: 5.3 MB (91.67%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.c
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.c
@@ -1,13 +1,13 @@
 int furthestDistanceFromOrigin(char * moves){
     int lodash = 0;
     int hor = 0;
-    
+
     for(char *move = moves; *move != '\0'; *move++){
         if(*move == '_'){
             lodash++;
         } else if(*move == 'L'){
             hor--;
-        } else if(*move == 'R'){
+        } else {
             hor++;
         }
     }
