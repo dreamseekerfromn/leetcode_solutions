4827f39d2c11604b2b960c6600f146f99f5d524b
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-07-07 16:21:26 -0400

    Time: 79 ms (14.44%) | Memory: 52.3 MB (17.11%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.js
+++ 735-asteroid-collision/asteroid-collision.js
@@ -5,25 +5,30 @@
 var asteroidCollision = function(asteroids) {
     let stack = [];
     for(let i = 0; i < asteroids.length; i++){
+        if(stack.length == 0){
+            stack.push(asteroids[i]);
+            continue;
+        }
         if(asteroids[i] > 0){
             stack.push(asteroids[i]);
-        } 
-        else if(stack.at(-1) < 0 || stack.length == 0){
-            stack.push(asteroids[i]);
-        } else if(Math.abs(asteroids[i]) == Math.abs(stack.at(-1))){
-            stack.pop();
         } else {
-            while(Math.abs(asteroids[i]) > Math.abs(stack.at(-1)) && stack.at(-1) > 0){
-                stack.pop();
-            }
-            if(stack.length == 0 || (asteroids[i] < 0 && stack.at(-1) < 0)){
+            if(stack.at(-1) < 0){
                 stack.push(asteroids[i]);
-            } else if(Math.abs(asteroids[i]) == Math.abs(stack.at(-1))){
-                stack.pop();
+            } else {
+                if(Math.abs(asteroids[i]) == Math.abs(stack.at(-1))){
+                    stack.pop();
+                } else if(Math.abs(asteroids[i]) > Math.abs(stack.at(-1))){
+                    while(Math.abs(asteroids[i]) > Math.abs(stack.at(-1)) && stack.at(-1) > 0){
+                        stack.pop();
+                    }
+                    if(stack.length == 0 || (asteroids[i] < 0 && stack.at(-1) < 0)){
+                        stack.push(asteroids[i]);
+                    } else if(Math.abs(asteroids[i]) == Math.abs(stack.at(-1))){
+                        stack.pop();
+                    }
+                }
             }
         }
-        
-        
     }
     return stack;
 };
