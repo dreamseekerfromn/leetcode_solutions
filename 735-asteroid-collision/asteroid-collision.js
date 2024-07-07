1e6b921b519c4c0abee81070dc6f1d15904161de
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-07-07 15:31:28 -0400

    Time: 59 ms (77.90%) | Memory: 52 MB (27.90%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    let stack = [];
+    for(let i = 0; i < asteroids.length; i++){
+        if(stack.length == 0){
+            stack.push(asteroids[i]);
+            continue;
+        }
+        if(asteroids[i] > 0){
+            stack.push(asteroids[i]);
+        } else {
+            if(stack.at(-1) < 0){
+                stack.push(asteroids[i]);
+            } else {
+                if(Math.abs(asteroids[i]) == Math.abs(stack.at(-1))){
+                    stack.pop();
+                } else if(Math.abs(asteroids[i]) > Math.abs(stack.at(-1))){
+                    while(Math.abs(asteroids[i]) > Math.abs(stack.at(-1)) && stack.at(-1) > 0){
+                        stack.pop();
+                    }
+                    if(stack.length == 0 || (asteroids[i] < 0 && stack.at(-1) < 0)){
+                        stack.push(asteroids[i]);
+                    } else if(Math.abs(asteroids[i]) == Math.abs(stack.at(-1))){
+                        stack.pop();
+                    }
+                }
+            }
+        }
+    }
+    return stack;
+};
