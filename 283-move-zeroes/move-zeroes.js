3ffa489780d9aea6c130d481beeb07466074aee6
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-08-06 19:27:54 -0400

    Time: 162 ms (6.52%) | Memory: 53.4 MB (86.68%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let index1 = 0;
+    let index2 = 1;
+    while(index1 < nums.length - 1){
+        if(nums[index1] != 0){
+            index1++;
+            index2 = index1 + 1;
+        } else {
+            if(nums[index2] != 0){
+                let temp = nums[index2];
+                nums[index2] = nums[index1];
+                nums[index1] = temp;
+            } else {
+                index2++;
+                if(index2 == nums.length){
+                    index1++;
+                    index2 = index1 + 1;
+                }
+            }
+        }
+    }
+};
