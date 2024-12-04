f9271c0d9d21fe1aa10dcf8acff364329d24ac6e
Sung Yi <10932336+dreamseekerfromn@users.noreply.github.com>
2024-12-04 17:26:30 -0500

    Time: 74 ms (5.45%) | Memory: 60 MB (5.38%) - LeetSync

---
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    let trav = function(node){
+        if(!node){
+            return null;
+        }
+        if(node.val == val){
+            return node;
+        }
+        console.log(node.val)
+        return trav(node.left) || trav(node.right);
+    }
+    return trav(root);
+};
